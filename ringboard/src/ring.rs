cff62e0e1651bc1e7fb7620794ddba0b4391ffb2
Alex Saveau <saveau.alexandre@gmail.com>
2024-02-14 15:09:10 -0800

    Fix memory leak on Ring open errors
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- ringboard/src/ring.rs
+++ ringboard/src/ring.rs
@@ -3,7 +3,7 @@ use std::{
     fs::File,
     io::{ErrorKind, IoSlice, Write},
     mem,
-    os::{fd::OwnedFd, unix::fs::FileExt},
+    os::{fd::AsFd, unix::fs::FileExt},
     ptr,
     ptr::NonNull,
     slice,
@@ -20,9 +20,9 @@ use crate::{Error, IoErr, Result};
 pub const MAX_ENTRIES: u32 = (1 << 20) - 1;
 
 pub struct Ring {
-    ptr: NonNull<u8>,
-    len: usize,
-    capacity: usize,
+    mem: Mmap,
+    len: u32,
+    capacity: u32,
 }
 
 const MAGIC: [u8; 3] = [0x4D, 0x18, 0x32];
@@ -108,13 +108,41 @@ impl BucketEntry {
     }
 }
 
+struct Mmap {
+    ptr: NonNull<u8>,
+    len: usize,
+}
+
+impl Mmap {
+    fn new<Fd: AsFd>(fd: Fd, len: usize) -> rustix::io::Result<Self> {
+        Ok(Self {
+            ptr: unsafe {
+                NonNull::new_unchecked(mmap(
+                    ptr::null_mut(),
+                    len,
+                    ProtFlags::READ,
+                    MapFlags::SHARED_VALIDATE,
+                    fd,
+                    0,
+                )?)
+            }
+            .cast(),
+            len,
+        })
+    }
+}
+
+impl Drop for Mmap {
+    fn drop(&mut self) {
+        let _ = unsafe { munmap(self.ptr.as_ptr().cast(), self.len) };
+    }
+}
+
 impl Ring {
     /// Open a Ringboard database.
     #[allow(clippy::missing_panics_doc)]
     pub fn open<P: Arg + Copy + Debug>(max_entries: u32, path: P) -> Result<Self> {
         let max_entries = max_entries.clamp(1, MAX_ENTRIES);
-        let capacity = usize::try_from(max_entries).unwrap() * mem::size_of::<RawEntry>()
-            + mem::size_of::<Header>();
 
         let fd = openat(CWD, path, OFlags::RDONLY, Mode::empty())
             .map_io_err(|| format!("Failed to open Ringboard database for reading: {path:?}"))?;
@@ -123,55 +151,43 @@ impl Ring {
             .map_io_err(|| "Failed to statx Ringboard database file.")?
             .stx_size;
         let len = usize::try_from(len).unwrap();
-        let ptr = unsafe {
-            NonNull::new_unchecked(
-                mmap(
-                    ptr::null_mut(),
-                    capacity,
-                    ProtFlags::READ,
-                    MapFlags::SHARED_VALIDATE,
-                    fd,
-                    0,
-                )
-                .map_io_err(|| "Failed to map memory.")?,
-            )
-        }
-        .cast::<u8>();
+        let mem = Mmap::new(fd, usize::try_from(entries_to_offset(max_entries)).unwrap())
+            .map_io_err(|| "Failed to map memory.")?;
 
-        if len < MAGIC.len() || unsafe { slice::from_raw_parts(ptr.as_ptr(), MAGIC.len()) } != MAGIC
+        if len < MAGIC.len()
+            || unsafe { slice::from_raw_parts(mem.ptr.as_ptr(), MAGIC.len()) } != MAGIC
         {
             return Err(Error::NotARingboard {
                 file: path.to_string_lossy().into_owned().into(),
             });
         }
 
-        Ok(Self { ptr, len, capacity })
+        Ok(Self {
+            mem,
+            len: offset_to_entries(len),
+            capacity: max_entries,
+        })
     }
 
-    pub fn len(&self) -> usize {
+    pub fn len(&self) -> u32 {
         self.len
     }
 
-    pub fn capacity(&self) -> usize {
+    pub fn capacity(&self) -> u32 {
         self.capacity
     }
 
     pub fn write_head(&self) -> u32 {
         let bytes = unsafe {
             slice::from_raw_parts(
-                self.ptr
+                self.mem
+                    .ptr
                     .as_ptr()
                     .add(MAGIC.len() + mem::size_of_val(&VERSION)),
                 mem::size_of::<u32>(),
             )
         };
         u32::from_le_bytes(bytes.try_into().unwrap())
-    }
-}
-
-impl Drop for Ring {
-    fn drop(&mut self) {
-        let _ = unsafe { munmap(self.ptr.as_ptr().cast(), self.capacity) };
     }
 }
 
@@ -222,7 +238,7 @@ impl RingWriter {
         self.ring
             .write_all_at(
                 &RawEntry::from(entry).0.to_le_bytes(),
-                u64::from(at) * u64::try_from(mem::size_of::<RawEntry>()).unwrap(),
+                entries_to_offset(at),
             )
             .map_io_err(|| format!("Failed to write entry to Ringboard database: {entry:?}"))
     }
@@ -235,4 +251,14 @@ impl RingWriter {
             )
             .map_io_err(|| format!("Failed to update Ringboard write head: {head}"))
     }
+}
+
+fn entries_to_offset(entries: u32) -> u64 {
+    u64::from(entries) * u64::try_from(mem::size_of::<RawEntry>()).unwrap()
+        + u64::try_from(mem::size_of::<Header>()).unwrap()
+}
+
+fn offset_to_entries(offset: usize) -> u32 {
+    u32::try_from(offset.saturating_sub(mem::size_of::<Header>()) / mem::size_of::<RawEntry>())
+        .unwrap()
 }
