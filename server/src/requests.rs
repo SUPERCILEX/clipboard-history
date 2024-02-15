04af305bfcf3006cfea50749d0475a023cf0357a
Alex Saveau <saveau.alexandre@gmail.com>
2024-02-14 21:05:48 -0800

    Simplify add FD parsing
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- server/src/requests.rs
+++ server/src/requests.rs
@@ -48,19 +48,16 @@ pub fn handle(
     info!("Processing request: {request:?}");
     match request {
         Request::Add => {
-            let mut fds = ArrayVec::<_, 1>::new();
+            let mut ids = ArrayVec::<_, 1>::new();
 
             for message in unsafe { AncillaryDrain::parse(control_data) } {
                 if let RecvAncillaryMessage::ScmRights(received_fds) = message {
-                    fds.extend(received_fds);
+                    for fd in received_fds {
+                        let id = allocator.add(fd, RingKind::Main, "")?;
+                        info!("Entry added: {id}");
+                        ids.push(id);
+                    }
                 }
-            }
-
-            let mut ids = ArrayVec::<_, 1>::new();
-            for fd in fds {
-                let id = allocator.add(fd, RingKind::Main, "")?;
-                info!("Entry added: {id}");
-                ids.push(id);
             }
 
             Ok(send_bufs
