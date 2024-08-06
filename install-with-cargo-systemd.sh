dbf114ed77555298d6dd9d066fec928819704236
Alex Saveau <saveau.alexandre@gmail.com>
2024-08-06 10:29:44 -0700

    Fix egui install using wrong features due to hack
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- install-with-cargo-systemd.sh
+++ install-with-cargo-systemd.sh
@@ -4,15 +4,15 @@ set -e
 cargo +nightly install clipboard-history-server --no-default-features --features systemd
 curl -s https://raw.githubusercontent.com/SUPERCILEX/clipboard-history/master/server/ringboard-server.service --create-dirs -O --output-dir ~/.config/systemd/user/
 
+cargo +nightly install clipboard-history
+cargo +nightly install clipboard-history-egui --no-default-features --features $XDG_SESSION_TYPE
+
 # TODO remove once wayland client is implemented
 XDG_SESSION_TYPE=x11
 cargo +nightly install clipboard-history-$XDG_SESSION_TYPE --no-default-features
 curl -s https://raw.githubusercontent.com/SUPERCILEX/clipboard-history/master/$XDG_SESSION_TYPE/ringboard-$XDG_SESSION_TYPE.service -O --output-dir ~/.config/systemd/user/
 systemctl --user daemon-reload
 systemctl --user enable ringboard-$XDG_SESSION_TYPE --now
-
-cargo +nightly install clipboard-history
-cargo +nightly install clipboard-history-egui --no-default-features --features $XDG_SESSION_TYPE
 
 echo
 echo "--- DONE ---"
