3c16b628d51c3d11b6f2082db63541c25a91169b
Alex Saveau <saveau.alexandre@gmail.com>
2024-08-20 22:49:55 -0700

    Improve service and desktop files by inlining paths, installing the desktop file, and adding an icon to it
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- install-with-cargo-systemd.sh
+++ install-with-cargo-systemd.sh
@@ -3,14 +3,22 @@ set -e
 
 cargo +nightly install clipboard-history-server --no-default-features --features systemd
 curl -s https://raw.githubusercontent.com/SUPERCILEX/clipboard-history/master/server/ringboard-server.service --create-dirs -O --output-dir ~/.config/systemd/user/
+sed -i "s|ExecStart=ringboard-server|ExecStart=$(which ringboard-server)|g" ~/.config/systemd/user/ringboard-server.service
 
 cargo +nightly install clipboard-history
+
 cargo +nightly install clipboard-history-egui --no-default-features --features $XDG_SESSION_TYPE
+curl -s https://raw.githubusercontent.com/SUPERCILEX/clipboard-history/master/egui/ringboard-egui.desktop --create-dirs -O --output-dir ~/.local/share/applications/
+curl -s https://raw.githubusercontent.com/SUPERCILEX/clipboard-history/master/logo.jpeg -o ringboard.jpeg --create-dirs -O --output-dir ~/.local/share/icons/hicolor/1024x1024/
+sed -i "s|Exec=ringboard-egui|Exec=$(echo /bin/sh -c \"ps -p \`cat /tmp/.ringboard/$USERNAME.egui-sleep 2\> /dev/null\` \> /dev/null 2\>\\\&1 \\\&\\\& exec rm -f /tmp/.ringboard/$USERNAME.egui-sleep \\\|\\\| exec $(which ringboard-egui)\")|g" ~/.local/share/applications/ringboard-egui.desktop
+sed -i "s|Icon=ringboard|Icon=$HOME/.local/share/icons/hicolor/1024x1024/ringboard.jpeg|g" ~/.local/share/applications/ringboard-egui.desktop
 
 # TODO remove once wayland client is implemented
 XDG_SESSION_TYPE=x11
 cargo +nightly install clipboard-history-$XDG_SESSION_TYPE --no-default-features
 curl -s https://raw.githubusercontent.com/SUPERCILEX/clipboard-history/master/$XDG_SESSION_TYPE/ringboard-$XDG_SESSION_TYPE.service -O --output-dir ~/.config/systemd/user/
+sed -i "s|ExecStart=ringboard-$XDG_SESSION_TYPE|ExecStart=$(which ringboard-$XDG_SESSION_TYPE)|g" ~/.config/systemd/user/ringboard-$XDG_SESSION_TYPE.service
+
 systemctl --user daemon-reload
 systemctl --user enable ringboard-$XDG_SESSION_TYPE --now
 
