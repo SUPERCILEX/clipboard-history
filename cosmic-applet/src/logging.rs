1af4c8547ca80476b21f9f4fe6515e1c1b725f43
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-10-17 18:45:32 +0200

    fix(cosmic-applet) added journalctl logging level filter

--- cosmic-applet/src/logging.rs
+++ cosmic-applet/src/logging.rs
@@ -1,14 +1,15 @@
-use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init_logging() {
-    let logger = tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer())
-        .with(EnvFilter::from_default_env());
+    let logger = tracing_subscriber::registry().with(tracing_subscriber::fmt::layer());
 
     #[cfg(not(debug_assertions))]
     logger
+        .with(tracing::level_filters::LevelFilter::INFO)
         .with(tracing_journald::layer().expect("Failed to open the journald connection"))
         .init();
     #[cfg(debug_assertions)]
-    logger.init();
+    logger
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
 }
